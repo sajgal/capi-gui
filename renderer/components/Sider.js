01dc5ad8596f193f038004c4de3218ff88a26df0
Matej Sajgal <roarbb@gmail.com>
2019-07-12 11:50:57 +1000

    Mobx integration

--- renderer/components/Sider.js
+++ renderer/components/Sider.js
@@ -36,7 +36,12 @@ const SiderComponent = () => {
         </Menu.Item>
         <Menu.Item key="2" href="/settings">
           <Link href="/settings">
-            <span className="nav-text"><Icon type="setting" /> Settings</span>
+            <span className="nav-text"><Icon type="control" /> Settings</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Link href="/mobx">
+            <span className="nav-text"><Icon type="medium" /> MobX Example</span>
           </Link>
         </Menu.Item>
       </Menu>
